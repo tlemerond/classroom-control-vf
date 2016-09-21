ca169a5e6b6181460d3613678094aa59ded21b3b
Teresa Lemerond <tlemerond@gmail.com>
2016-09-21 17:11:01 -0500

    fix

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,20 +1,17 @@
 class users {
      user { 'fundamentals':
        ensure => present,
-       uid => '5002',
        gid => '100',
        home => '/home/fundamentals'
        }
      user { 'cartman':
        ensure => present,
        gid => '100',
-       uid => '5000',
        home => '/home/cartman'
        }
      user { 'finch':
        ensure => present,
        gid => '100',
-       uid => '5001',
-       home => 'home/finch'
+       home => '/home/finch'
        }
 }
