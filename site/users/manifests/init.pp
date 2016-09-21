d3cd3f684e21b874c5301d2a811742794b4f75d2
Teresa Lemerond <tlemerond@gmail.com>
2016-09-21 16:16:45 -0500

    added more users to users class

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -2,4 +2,14 @@ class users {
      user { 'fundamentals':
        ensure => present
        }
+     user { 'cartman':
+       ensure => present,
+       gid => '10',
+       uid => '5000'
+       }
+     user { 'finch':
+       ensure => present,
+       gid => '10',
+       uid => '5001'
+       }
 }
