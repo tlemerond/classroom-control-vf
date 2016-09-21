a8b397037d4cfe6c4b6e07f3637f0c2c44c52ad6
Teresa <tlemerond@gmail.com>
2016-09-21 14:27:37 -0500

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,9 +1,5 @@
 class users {
      user { 'fundamentals':
-       ensure => present,
-       allowdupe => false,
-       username => "fundamentals",
-       gid => '100',
-       uid => '5000'
+       ensure => present
        }
 }
