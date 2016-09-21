ae3d0ab0af9fa7eb97c096f63b6d60d5d1b889b8
Teresa Lemerond <tlemerond@gmail.com>
2016-09-21 17:03:52 -0500

    fixed users' dirs

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,15 +1,20 @@
 class users {
      user { 'fundamentals':
        ensure => present
+       home => '/home/fundamentals',
+       uid => '5002',
+       gid => '100',
        }
      user { 'cartman':
        ensure => present,
-       gid => '10',
+       gid => '100',
        uid => '5000'
+       home => '/home/cartman',
        }
      user { 'finch':
        ensure => present,
-       gid => '10',
+       gid => '100',
        uid => '5001'
+       home => 'home/finch',
        }
 }
