9a2a8586eb9409aa47fd03f0cddea0fad0c9c237
Teresa Lemerond <tlemerond@gmail.com>
2016-09-21 17:14:48 -0500

    added managehome

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -2,16 +2,19 @@ class users {
      user { 'fundamentals':
        ensure => present,
        gid => '100',
+       managehome => 'true',
        home => '/home/fundamentals'
        }
      user { 'cartman':
        ensure => present,
        gid => '100',
+       managehome => 'true',
        home => '/home/cartman'
        }
      user { 'finch':
        ensure => present,
        gid => '100',
-       home => '/home/finch'
+       home => '/home/finch',
+       managehome => 'true'
        }
 }
