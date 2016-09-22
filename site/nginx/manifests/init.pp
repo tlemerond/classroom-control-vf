e9edfbf1dfc8d87ce9b3c1fbf57c0d3deec3c85c
Teresa Lemerond <tlemerond@gmail.com>
2016-09-22 16:46:15 -0500

    fixed stuff

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,28 +1,46 @@
-class nginx { 
-
-   package { 'nginx':
-      ensure => present,
-   }
-
-   service { 'nginx':
-      ensure => running,
-      enable => true,
-      require => File['/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf'],
-   }
-
-
-   file { '/etc/nginx/nginx.conf':
-      source => 'puppet:///modules/nginx/nxginx.conf', 
-      owner => 'root',
-      group => 'root',
-      mode => '0444',
-   }
-
-   file { '/etc/nginx/conf.d/default.conf':
-      source => 'puppet:///modules/nginx/default.conf',
-      owner => 'root',
-      group => 'root',
-      mode => '0444',
-   }
-
-}   
+class nginx {
+  package { 'nginx':
+    ensure => present,
+}
+  file { '/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
