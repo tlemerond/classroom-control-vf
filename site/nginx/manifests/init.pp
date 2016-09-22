4c1dd37fb8041f222da0070c6c1ddf92d35b21f3
Teresa Lemerond <tlemerond@gmail.com>
2016-09-22 16:35:39 -0500

    fix

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,27 +1,28 @@
-class { 
+class nginx { 
+
    package { 'nginx':
-       ensure => present,
+      ensure => present,
    }
 
    service { 'nginx':
-       ensure => running,
-       enable => true,
-       require => File['/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf'],
+      ensure => running,
+      enable => true,
+      require => File['/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf'],
    }
 
 
    file { '/etc/nginx/nginx.conf':
-       source => 'puppet:///modules/nginx/nginx/nxginx.conf', 
-       owner => 'root',
-       group => 'root',
-       mode => '0444',
+      source => 'puppet:///modules/nginx/nxginx.conf', 
+      owner => 'root',
+      group => 'root',
+      mode => '0444',
    }
 
    file { '/etc/nginx/conf.d/default.conf':
-       source => 'puppet:///modules/nginx/conf.d/default.conf',
-       owner => 'root',
-       group => 'root',
-       mode => '0444',
+      source => 'puppet:///modules/nginx/default.conf',
+      owner => 'root',
+      group => 'root',
+      mode => '0444',
    }
 
 }   
