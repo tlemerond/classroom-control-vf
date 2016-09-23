c12afe817365899d617da83ce87170d8f629f0bd
tlemerond@gmail.com <tlemerond@gmail.com>
2016-09-23 16:39:02 -0400

    Update and fix

---
+++ site/users/manifests/important_user.pp
@@ -0,0 +1,4 @@
+class user::important_user {
+    user::managed_user("root_bob":),
+    user::managed_user("root_ann":),
+}
--- site/users/manifests/important_users.pp
+++
@@ -1,4 +0,0 @@
-class important_users {
-    users::managed_users("root_bob":),
-    users::managed_users("root_ann":),
-}
