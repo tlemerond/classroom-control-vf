f77f88864edd0323bc09125af87350336efab0e7
Teresa Lemerond <tlemerond@gmail.com>
2016-09-23 12:34:52 -0400

    refactored

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,38 +2,29 @@ class nginx {
   package { 'nginx':
     ensure => present,
 }
-  file { '/var/www':
-    ensure => directory,
+  File {
     owner => 'root',
     group => 'root',
-    mode => '0775',
   }
+
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
+    mode => '0664',
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
+    mode => '0664',
   }
-  file { '/etc/nginx/conf.d':
+  file { ['/etc/nginx/conf.d', '/var/www'] :
     ensure => directory,
-    owner => 'root',
-    group => 'root',
     mode => '0775',
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
     mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
