4d6b6c8fdc809cdbcfc344d10085e317cb569b9b
tlemerond@gmail.com <tlemerond@gmail.com>
2016-09-23 13:07:20 -0400

    added conditional statement

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,7 +50,7 @@ node default {
         
   if $::virtual != 'physical' {
       $vmname = capitalize($::virtual)
-      notify{ "This is a ${vmname} virtual machine.":}
+      notify {"This is a ${vmname} virtual machine.":}
   }
   include memcached
   include nginx
